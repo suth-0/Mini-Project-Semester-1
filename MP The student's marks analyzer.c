df47763e6158469537cbce961a53bbc2446e799a
suth-0 <sushmathokchom25@gmail.com>
2025-11-29 22:59:05 +0530

    Update MP The student's marks analyzer.c

--- MP The student's marks analyzer.c
+++ MP The student's marks analyzer.c
@@ -1,3 +1,48 @@
+Question: The Student’s Marks Analyzer
+
+💡 Problem Story:
+A teacher wants to analyze the marks of her students in a test.
+She asks you to write a program that stores the marks of n students and finds out:
+
+The total marks obtained by all students.
+
+The average marks of the class.
+
+Can you help the teacher by writing the program?
+Task:
+
+Write a program that takes the number of students and their marks as input and calculates:
+
+The total marks
+
+The average marks
+
+Input Format:
+
+The first line contains one integer n — number of students.
+
+The next line contains n integers — marks of each student.
+
+📤 Output Format:
+
+Print two lines:
+
+First line: "Total Marks = <value>"
+
+Second line: "Average Marks = <value>"
+Example 1:
+
+Input:
+5
+80 70 90 85 75
+
+Output:
+Total Marks = 400
+Average Marks = 80.00
+
+
+
+
 #include<stdio.h>
 int main(){
     int n,i;
@@ -11,7 +56,8 @@ int main(){
         Total= Total+marks;
     }
     Average = Total/n;
-    printf("Total Marks=%.2f\n",Total);
-    printf("Average Marks=%.2f",Average);
+    printf("Total Marks = %.2f\n",Total);
+    printf("Average Marks = %.2f",Average);
     return 0;
+
 }
