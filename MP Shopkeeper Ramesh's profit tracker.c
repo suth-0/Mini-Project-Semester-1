a9a7cd4a7f8b1ce448785d9ec4080842e2cae010
suth-0 <sushmathokchom25@gmail.com>
2025-11-29 22:50:48 +0530

    Update MP Shopkeeper Ramesh's profit tracker.c

--- MP Shopkeeper Ramesh's profit tracker.c
+++ MP Shopkeeper Ramesh's profit tracker.c
@@ -1,3 +1,54 @@
+Problem Title: Shopkeeper Ramesh’s Profit Tracker 💰
+🧩 Story:
+
+Ramesh owns a small grocery shop in his town.
+He keeps a record of his daily profit or loss for a week in an array.
+
+If the value is positive, it means he made a profit that day.
+
+If the value is negative, it means he had a loss that day.
+
+At the end of the week, Ramesh wants to know:
+
+His total profit (sum of all positive values)
+
+His total loss (sum of all negative values, as a positive number)
+
+His net balance (profit + loss, considering signs)
+
+Help Ramesh write a program to calculate these values.
+
+Problem Statement:
+
+Given an array records[] of size n, where each element represents the daily profit or loss,
+find and print the total profit, total loss, and net balance.
+
+Input Format:
+
+The first line contains an integer n — number of days.
+
+The second line contains n space-separated integers representing profit or loss on each day.
+
+Output Format:
+
+Print three lines:
+
+Total Profit: X
+Total Loss: Y
+Net Balance: Z
+
+
+Where:
+
+X → sum of all positive numbers
+
+Y → sum of all negative numbers converted to positive value
+
+Z → X − Y
+
+
+
+
 #include <stdio.h>
 int main(){
     int n,i;
@@ -16,8 +67,9 @@ int main(){
         L=L+arr[i];
     }
     T=P+L;
-    printf("Profit=%d\n",P);
-    printf("Loss=%d\n",L);
-    printf("Total=%d",T);
+    printf("Total Profit: %d\n",P);
+    printf("Total Loss: %d\n",L);
+    printf("Net Balance: %d",T);
     return 0;
+
 }
