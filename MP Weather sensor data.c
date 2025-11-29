8467178cdc5c85498df4ef696aec1392e2e623d9
suth-0 <sushmathokchom25@gmail.com>
2025-11-29 22:59:43 +0530

    Update MP Weather sensor data.c

--- MP Weather sensor data.c
+++ MP Weather sensor data.c
@@ -1,3 +1,38 @@
+Problem 2: Count Positive and Negative Numbers (Weather Sensor Data)
+🧩 Problem Story:
+
+A weather station records temperature readings during the day.
+Positive readings mean above 0°C, negative readings mean below 0°C, and zero means exactly 0°C.
+
+Your job is to analyze the data and find how many readings are positive, negative, and zero.
+
+Input Format:
+
+The first line contains an integer n — the number of readings.
+
+The second line contains n space-separated integers representing the readings.
+
+🖥️ Output Format:
+
+Print three lines as shown:
+Positive = x
+Negative = y
+Zero = z
+
+Example 1:
+
+Input
+6
+3 -2 0 7 -5 8
+
+Output
+Positive = 3
+Negative = 2
+Zero = 1
+
+
+
+
 #include <stdio.h>
 int main(){
     int n,i;
@@ -21,4 +56,5 @@ int main(){
     printf("Negative = %d\n",N);
     printf("Zero = %d\n",Z);
     return 0;
+
 }
